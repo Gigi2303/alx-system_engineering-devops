ee6cb23544b5919bcc37078a8f8198a1589cc497
Gigi2303 <ajibola.grace201@gmail.com>
2024-05-07 15:13:17 -0700

    debug

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-"
-a script that Writes a function that queries the Reddit API and returns the number of subscribers
-"
 
 import requests
 
-def number_of_subscribers(subreddit);
+""
+a script that Writes a function that queries the Reddit API
+and returns the number of subscribers
+""
 
-url = f"https://www.reddit.com/r/{subreddit}/about.json"
-response = requests.get(url, headers=headers, allow_redirects=False)
 
-if response.status_code == 200:
-    data = response.json()
-    return data['data']['subscribers']
-else
+def number_of_subscribers(subreddit):
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
+
+    response = requests.get(url.format(subreddit), headers=headers,
+                            allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else
     return 0
